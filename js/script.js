16b7887a7c0ed722c65f185c52a33dc23da4ed34
lirije <lirije11@hotmail.com>
2023-08-31 22:27:08 +0200

    some parts of home page done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function toggleMenu(attribute) {
+    try {
+        const toggleBtn = document.querySelector(attribute)
+        toggleBtn.addEventListener('click', function () {
+            const element = document.querySelector(toggleBtn.getAttribute('data-target'))
+
+            if (!element.classList.contains('open-menu')) {
+                element.classList.add('open-menu')
+                toggleBtn.classList.add('btn-outline-dark')
+                toggleBtn.classList.add('btn-toggle-white')
+            }
+
+            else {
+                element.classList.remove('open-menu')
+                toggleBtn.classList.remove('btn-outline-dark')
+                toggleBtn.classList.remove('btn-toggle-white')
+            }
+        })
+    } catch { }
+}
+toggleMenu('[data-option="toggle"]')
